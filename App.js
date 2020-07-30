bf5dc3371fe6701de119a132fe4a5b526b953461
vhamilton08 <67924236+vhamilton08@users.noreply.github.com>
2020-07-30 01:01:56 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Header from "./Components/Header"
+import './App.css';
+import Array from "./Components/Array"
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <div class="body">
+      <Array />
+      </div>
+    </div>
+
+  )
+}
+
+export default App
